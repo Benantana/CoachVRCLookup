f084218e07c13cc74412f55e5cb0d0c2dcc91407
Benas Antanavicius <antanavicius@icloud.com>
2025-02-19 10:31:00 -0500

    Added explanation

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()  # Load environment variables from .env file. You need to make this and add the key to it like this: ROBOTEVENTS_API_KEY=Bearer your_token_here
 
 ROBOTEVENTS_API_KEY = os.getenv('ROBOTEVENTS_API_KEY')
